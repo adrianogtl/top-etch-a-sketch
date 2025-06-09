d2e5249492a675088acef50d14f8cd8efabba870
Adriano Garcia <80999356+adrianogtl@users.noreply.github.com>
2025-06-09 12:24:39 -0300

    rework grid size and clear canvas features

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,34 @@
 const canvas = document.querySelector(".canvas");
-const resetBtn = document.querySelector("#reset-btn");
+const gridSizeInput = document.querySelector("#grid-size-input");
+const gridSizeSpan = document.querySelector("#grid-size-span");
+const clearBtn = document.querySelector("#clear-btn");
 const colorSelector = document.querySelector("#pencil-color");
+
+const getGrid = () => Array.from(canvas.querySelectorAll("div"));
+
 let paintBrushColor = colorSelector.value;
-
-resetBtn.addEventListener("click", resetCanvas);
-colorSelector.addEventListener("change", changePaintBrushColor);
-
-function resetCanvas() {
-  deleteGrid();
-  let gridSize = 16;
-  do {
-    gridSize = parseInt(prompt("Grid size: (min: 16  max: 100)"));
-  } while (isNaN(gridSize) || gridSize > 100 || gridSize < 16);
-  createGrid(gridSize);
-}
 
 function changePaintBrushColor(element) {
   paintBrushColor = element.target.value;
 }
 
+function changeGridSize(gridSize) {
+  if (gridSize < 16 || gridSize > 50) {
+    return;
+  }
+
+  deleteGrid();
+  createGrid(gridSize);
+}
+
 function createGrid(gridSize = 16) {
+  const canvasWidth = canvas.clientWidth;
   const totalSize = gridSize * gridSize;
   for (let i = 0; i < totalSize; i++) {
     const pixel = document.createElement("div");
-    const pixelSize = `${800 / gridSize}px`;
+    const pixelSize = `${canvasWidth / gridSize}px`;
     pixel.style.width = pixelSize;
-    pixel.style.heigth = pixelSize;
+    pixel.style.height = pixelSize;
     pixel.addEventListener("mousemove", (event) => {
       if (event.buttons == 1) {
         event.preventDefault();
@@ -38,7 +41,21 @@ function createGrid(gridSize = 16) {
 }
 
 function deleteGrid() {
-  const pixels = Array.from(canvas.querySelectorAll("div"));
+  const pixels = getGrid();
   pixels.forEach((pixel) => pixel.remove());
 }
+
+function clearGrid() {
+  const pixels = getGrid();
+  pixels.forEach((pixel) => (pixel.style.backgroundColor = "#fff"));
+}
+
+gridSizeInput.addEventListener("input", () => {
+  const gridSize = gridSizeInput.value;
+  changeGridSize(gridSize);
+  gridSizeSpan.textContent = `${gridSize} x ${gridSize}`;
+});
+clearBtn.addEventListener("click", clearGrid);
+colorSelector.addEventListener("change", changePaintBrushColor);
+
 createGrid();
